fe33aa32b36cbc365dcdfa8a1d987bec6b040aac
Harshit-077 <harshitsharma.0777@gmail.com>
2025-02-22 16:40:39 +0530

    Slight updates

--- tasks/views.py
+++ tasks/views.py
@@ -10,7 +10,8 @@ class newTaskForm(forms.Form):
     #priority = forms.IntegerField(label = "Priority",min_value=1,max_value=4)
 # Create your views here.
 def index(request):
-    
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
 
     return render(request,"tasks/index.html",{
         #"tasks":tasks
